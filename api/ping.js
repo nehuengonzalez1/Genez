5da855533b12ba521ddbff66de7180e7a70da93b
nehuengonzalez1 <nehuengonzalez1@gmail.com>
2026-08-09 14:44:31 -0300

    Quitar la funcion de prueba

--- api/ping.js
+++
@@ -1,4 +0,0 @@
-// Diagnóstico temporal: confirma si Vercel está publicando la carpeta api/
-export default function handler(req, res) {
-  res.status(200).json({ ok: true, ruta: "api/ping", metodo: req.method });
-}
